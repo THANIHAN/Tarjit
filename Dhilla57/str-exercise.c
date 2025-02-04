ed072e1b51a322caab8de49a20e545c0658e602b
adhill618 <adhill618@gmail.com>
2025-02-04 17:41:12 -0500

    Completed string tasks

--- Dhilla57/str-exercise.c
+++ Dhilla57/str-exercise.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
-# include <string.h>
+#include <string.h>
 int starts_with(const char *str, const char *sub);
+int count = 0;
 int main () {
-    printf("%d\n", starts_with("aahahaha", "aah"));
+    printf("%d\n", starts_with("aahahaha", "ah"));
     return 0;
 }
 
 int starts_with(const char *str, const char *sub){
     for(int i= 0; i<strlen(str); i++){
-        if (str[i] == sub[i]);
-        return 1;
+        if (str[i] == sub[i]){
+            count = count+1;
+            if(count == strlen(sub)){
+                return 1;
+            }
+        }else{
+            return 0;
+        }
     }
 }
